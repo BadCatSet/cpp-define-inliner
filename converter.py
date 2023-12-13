5846e02f84742efdf7b643589ffc1be46cdb1a63
BadCatSet <leokostarev@mail.ru>
2023-12-13 21:25:20 +0300

    init

---
+++ converter.py
@@ -0,0 +1,137 @@
+from string import ascii_letters, digits
+
+VALID_CHARS = ascii_letters + digits + "_"
+
+
+class Define:
+    """
+    A define in c++ program.
+    """
+
+    def __init__(self, raw: str):
+        raw = raw[8:]
+
+        self.name = ""
+        self.arguments = []
+        self.body = ""
+
+        for i in range(len(raw)):
+            if raw[i] not in VALID_CHARS:
+                self.name = raw[:i]
+                name_end = i
+                break
+        else:
+            raise ValueError("Invalid define: " + raw)
+
+        if raw[name_end] == " ":
+            self.body = raw[i + 1:]
+            return
+
+        args_start = name_end + 1
+        args_end = raw.find(")", args_start)
+        self.body = raw[args_end + 2:]
+
+        self.arguments = [x.strip() for x in raw[args_start:args_end].split(",")]
+
+    def tune(self, program: str):
+        if len(self.arguments) == 0:
+            return self.standard_tune(program)
+        return self.arg_tune(program)
+
+    def standard_tune(self, program: str):
+        min_pos = 0
+        while True:
+            pos = program.find(self.name, min_pos)
+            if pos == -1:
+                break
+            if program[pos - 1] in VALID_CHARS:
+                min_pos = pos + 1
+                continue
+            if program[pos + len(self.name)] in VALID_CHARS:
+                min_pos = pos + 1
+                continue
+            program = program[:pos] + self.body + program[pos + len(self.name):]
+            min_pos = pos + len(self.body)
+        return program
+
+    def arg_tune(self, program: str):
+        min_pos = 0
+        while True:
+            pos = program.find(self.name, min_pos)
+            if pos == -1:
+                break
+            if program[pos - 1] in VALID_CHARS:
+                min_pos = pos + 1
+                continue
+            if program[pos + len(self.name)] in VALID_CHARS:
+                min_pos = pos + 1
+                continue
+
+            args_start = program.find("(", pos)
+            brackets = 1
+            args = []
+            buffer = ""
+            for i in range(args_start + 1, len(program)):
+                if program[i] == "(":
+                    brackets += 1
+                elif program[i] == ")":
+                    brackets -= 1
+                if brackets == 0:
+                    if buffer:
+                        args.append(buffer)
+                    define_end = i
+                    break
+                buffer += program[i]
+                if program[i] == "," and brackets == 1:
+                    args.append(buffer)
+                    buffer = ""
+
+            else:
+                raise ValueError(f"INVALID TUNE AT {pos}\n IN" + program)
+
+            if len(args) != len(self.arguments):
+                raise ValueError(f"Argument count mismatch: {len(self.arguments)} needed but {len(args)} found")
+
+            prepared_body = self.body
+
+            for fr, to in zip(self.arguments, args):
+                prepared_body = prepared_body.replace(fr, to)
+
+            program = program[:pos] + prepared_body + program[define_end + 1:]
+            min_pos = pos + len(self.body)
+
+        return program
+
+
+def convert(program: str):
+    """
+    Inlines all defines in given c++ program.
+
+    Args:
+        program (str): The c++ program to be converted.
+
+    Returns:
+        str: The converted c++ program.
+    """
+    defines = []
+
+    while True:
+        pos = program.find("#define")
+        if pos == -1:
+            break
+        end_pos = program.find("\n", pos)
+
+        defines.append(Define(program[pos:end_pos]))
+
+        program = program[:pos] + program[end_pos + 1:]
+
+    for define in defines:
+        program = define.tune(program)
+    return program
+
+
+if __name__ == "__main__":
+    with open("raw.cpp") as f:
+        data = f.read()
+    data = convert(data)
+    print(data)
---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+from converter import convert
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello from Flask!'
+
+
+@app.route('/api', methods=['POST'])
+def api():
+    try:
+        data = str(request.data)
+        data = convert(data)
+        return data
+    except Exception as e:
+        return str(e)
+
+
+app.run(host='0.0.0.0', port=81)
